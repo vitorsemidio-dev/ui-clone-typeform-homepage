5f588ac6db40eec6ed638f5459da49ab61ab5735
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-09-15 23:26:22 -0300

    Adição libs e ajustes arquivos iniciais

---
+++ src/styles/global.ts
@@ -0,0 +1,9 @@
+import { createGlobalStylee } from 'styled-components';
+
+export default createGlobalStylee`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+`;
