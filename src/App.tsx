8d46d3f0bfd58c2a089327c12f5d1ec3f2d6776f
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-09-15 23:43:43 -0300

    Cores e estrutura para animacao

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,21 @@ function App() {
   return (
     <>
       <GlobalStyle />
-      Typeform
+      
+      <Main style={{ height: '1610vh' }}>
+        <Section style={{ height: '72.7%' }}>
+          <Sticky className="first" />
+          <Sticky className="second" />
+        </Section>
+
+        <Section style={{ height: '9.7%' }}>
+          <Sticky className="third" />
+        </Section>
+
+        <Section style={{ height: '10.1%' }}>
+          <Sticky className="fourth" />
+        </Section>
+      </Main>
     </>
   );
 }
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,10 +1,45 @@
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
 
-export const Main = styled.main``;
+export const Main = styled.main`
+    .first {
+        overflow: hidden;
+        background: red;
 
-export const Section = styled.section``;
+        .offset {}
+
+        .a {
+            background: aqua;
+        }
+        .b {
+            background: darkblue;
+        }
+        .c {
+            background: blue;
+        }
+    }
+
+    .second {
+        background: green;
+    }
+
+    .third {
+        background: yellow;
+    }
+
+    .fourth {
+        background: purple;
+    }
+`;
+
+export const Section = styled.section`
+    position: relative;
+`;
 
 export const Sticky = styled.div`
-    
+    height: 100vh;
+    position: sticky;
+    top: 0;
+    left: 0;
+    right: 0;
 `;
